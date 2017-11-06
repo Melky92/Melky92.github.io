1f3cd06490d8dde6a9a798a78f1403f8952d185e
melky92 <melkydominguez@gmail.com>
2017-11-06 16:32:35 -0400

    tiempo inicial en tiempo actual

--- calcs.js
+++ calcs.js
@@ -2,6 +2,26 @@
 $(document).ready(function () {
     new Clipboard('#copiar_resultado');
     //document.getElementById("input_hora_imagen").value = new Date($.now());
+    $('input[type="time"][value="now"]').each(function(){
+    var d = new Date(),
+        h = d.getHours(),
+        m = d.getMinutes();
+    m = m - 10;
+    if(m < 0)
+    {
+      m = m + 60;
+      h = h - 1;
+      if(h < 0)
+      {
+        h = h + 24;
+      }
+    }
+    if(h < 10) h = '0' + h;
+    if(m < 10) m = '0' + m;
+    $(this).attr({
+      'value': h + ':' + m
+    });
+  });
 });
 
 function CheckAndRun(){
