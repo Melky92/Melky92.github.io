f5340a104bed6af93dedbb0a5e60763112506b9a
Melky Dominguez <melky92@hotmail.com>
2019-12-24 16:17:14 -0400

    mover pagina pkmn

---
+++ pkmn/calcs.js
@@ -0,0 +1,113 @@
+
+$(document).ready(function () {
+    new Clipboard('#copiar_resultado');
+    //document.getElementById("input_hora_imagen").value = new Date($.now());
+    $('input[type="time"][value="now"]').each(function(){
+    var d = new Date(),
+        h = d.getHours(),
+        m = d.getMinutes();
+    m = m - 10;
+    if(m < 0)
+    {
+      m = m + 60;
+      h = h - 1;
+      if(h < 0)
+      {
+        h = h + 24;
+      }
+    }
+    if(h < 10) h = '0' + h;
+    if(m < 10) m = '0' + m;
+    $(this).attr({
+      'value': h + ':' + m
+    });
+  });
+});
+
+function CheckAndRun(){
+  //var hora_imagen = new Date($("#input_hora_imagen")[0].value);
+  //var tiempo_restante = new Date($("#input_tiempo_restante")[0].value);
+  var hora_actual = new Date($.now()).getTime() % (100800000) + (14400000)*3;
+  var hora_imagen = document.getElementById("input_hora_imagen").valueAsNumber + (14400000);
+  var tiempo_restante_img = document.getElementById("input_tiempo_restante").valueAsNumber;// * 60000;
+  var eclosionado = $("#input_eclosionado")[0].checked;
+
+  console.log(hora_actual / 3600000);
+  console.log(hora_imagen / 3600000);
+
+  if(isNaN(hora_imagen) || isNaN(tiempo_restante_img))
+  {
+    console.log("missing data");
+    return;
+  }
+
+  if(eclosionado == false)
+  {
+    tiempo_restante_img = tiempo_restante_img + 45;
+  }
+//50
+//30
+//20
+  var tiempo_raid = null;
+  var tiempo_rezagados = null;
+  var tiempo_restante = tiempo_restante_img - (hora_actual - hora_imagen)/60000;
+
+  if(tiempo_restante <= 20)
+  {
+    tiempo_raid = tiempo_restante - 5;
+  }
+  else if(tiempo_restante <= 40)
+  {
+    tiempo_raid = tiempo_restante - 10;
+  }
+  else if(tiempo_restante <= 45)
+  {
+    tiempo_raid = 30;
+  }
+  else if(tiempo_restante <= 65)
+  {
+    tiempo_raid = 30;
+    tiempo_rezagados = 50;
+  }
+  else
+  {
+    tiempo_raid = tiempo_restante - 40;
+    tiempo_rezagados = tiempo_restante - 20;
+  }
+  
+
+  var tiempo_raid = (hora_actual + (tiempo_raid * 60000));
+  var tiempo_rezagados = (hora_actual + (tiempo_rezagados * 60000));
+  var tiempo_fin = (hora_imagen + (tiempo_restante_img * 60000));
+
+  
+  var PKMN = $("#input_pkmn")[0].selectedOptions[0];
+  var GYM = document.getElementById("input_gym_name").value;
+  var DIR = document.getElementById("input_gym_dir").value;
+  var TIME_RAID = new Date(tiempo_raid).toTimeString().split(' ')[0].substring(0,5);
+  //var TIME_RAID = new Date((hora_imagen)).toTimeString().split(' ')[0].substring(0,5);
+  var TIME_RAID2 = new Date(tiempo_rezagados).toTimeString().split(' ')[0].substring(0,5);
+  var TIME_END = new Date(tiempo_fin).toTimeString().split(' ')[0].substring(0,5);
+
+  var resultado = "";
+  resultado += "RAID " + PKMN.name + "\n";
+  for(i = 0; i< PKMN.level;i++)
+    resultado += PKMN.icon;
+  resultado += "\n";
+  resultado += "GYM " + GYM + "\n";
+  resultado += "\n";
+  resultado += DIR + "\n";
+  resultado += "Hora raid: " + TIME_RAID + "\n";
+  resultado += "Se va: " + TIME_END + "\n";
+  resultado += "Quorum necesario: " + PKMN.rec_low + " a " + PKMN.rec_high + " personas\n";
+  resultado += "\n";
+  resultado += "\n";
+  resultado += "Lista de confirmados:\n";
+  for(i = 1; i <= PKMN.rec_high;i++)
+    resultado += i + ")\n";
+  resultado += "\n";
+  resultado += "*Lista Oficial*\n";
+  resultado += "Suerte Gente!\n";
+
+  $("#texto_resultado").text(resultado);
+}
